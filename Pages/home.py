8f66dd16ea308b227c2f5bdbd73103bdcd85e716
BekaluTa <bekiman21@gmail.com>
2022-08-26 14:16:37 +0300

    adding git ignore directory

---
+++ Pages/home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+
+def home_app():
+    st.title("Home")
+    st.write(
+        "A detail analysis of telecom data's customer engagement, experience, and overview")
